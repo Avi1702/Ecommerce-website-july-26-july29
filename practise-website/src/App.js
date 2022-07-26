dd0b77aab053c5a3c7c55f3a04aa8391f3ca66b2
Avi1702 <avinashroy1702@gmail.com>
2022-07-27 02:08:20 +0530

    day-1

---
+++ practise-website/src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+// import SideDrawer from './pages/Drawer';
+import { Login } from './pages/Login';
+import {Routes, Route} from "react-router-dom"
+import { Home } from './pages/Home';
+import { Private } from './components/Private';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      
+      <Routes>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/" element={<Private><Home/></Private>}></Route>
+      </Routes>
+  
+    </div>
+  );
+}
+
+export default App;
